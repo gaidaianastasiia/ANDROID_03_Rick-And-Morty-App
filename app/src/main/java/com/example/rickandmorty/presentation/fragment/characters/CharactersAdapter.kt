1c9f42c71f5445c43d59b65f5d5f681cf8fc5928
Anastasia <haidaianastasiia@gmail.com>
2021-08-31 18:59:17 +0300

    The layout has been fixed

--- app/src/main/java/com/example/rickandmorty/presentation/fragment/characters/CharactersAdapter.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/fragment/characters/CharactersAdapter.kt
@@ -5,7 +5,6 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.example.rickandmorty.R
 import com.example.rickandmorty.databinding.CharactersListItemLayoutBinding
 import com.example.rickandmorty.entity.Character
 import com.squareup.picasso.Picasso
